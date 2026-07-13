36c7cc2a76a8195461f3bbd847e47641fbe7ef45
dev-masoud <masoudrezaie1999@gmail.com>
2026-07-13 17:44:12 +0330

    Login Test admin and user

--- TestScripts/Test_LoginClass.py
+++ TestScripts/Test_LoginClass.py
@@ -17,7 +17,7 @@ def setup():
     yield driver
     driver.quit()
 
-def test_valid_login(setup):
+def test_valid_admin_login(setup):
     driver=setup
     Login_page = LoginPage(driver)
     Login_page.login("supervisor","1")
@@ -28,10 +28,37 @@ def test_valid_login(setup):
     )
     assert main_menu.is_displayed(),"login should be successful but it is not"
 
-def test_invalid_login(setup):
+def test_invalid_admin_login(setup):
     driver=setup
     Login_page = LoginPage(driver)
     Login_page.login("supervisor","1111")
+
+    wait = WebDriverWait(driver, 5)
+    actual_text = wait.until(
+    EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, ".login-alert.ng-binding")
+    )
+    ).text
+    expected_text = "نام کاربری و یا کلمه عبور اشتباه است."
+
+    assert expected_text in actual_text,"login should be failed but it is not"
+
+
+def test_valid_user_login(setup):
+    driver=setup
+    Login_page = LoginPage(driver)
+    Login_page.login("user2","1")
+
+    wait = WebDriverWait(driver, 5)
+    main_menu = wait.until(
+    EC.visibility_of_element_located((By.XPATH, "//a[@class='user-panel']"))
+    )
+    assert main_menu.is_displayed(),"login should be successful but it is not"
+
+def test_invalid_user_login(setup):
+    driver=setup
+    Login_page = LoginPage(driver)
+    Login_page.login("user2","1111")
 
     wait = WebDriverWait(driver, 5)
     actual_text = wait.until(
