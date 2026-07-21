50b12e6e6fccb48cd9183221464f6db786a4a358
dev-masoud <masoudrezaie1999@gmail.com>
2026-07-21 21:23:52 +0330

    createUser def, still not working

--- PageObjects/Create_User_POM.py
+++ PageObjects/Create_User_POM.py
@@ -2,67 +2,134 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from Components.Main_Menu import MainMenu
+from selenium.webdriver.support.select import Select
 
 class CreateUserPage:
     def __init__(self,driver):
         self.driver = driver
-        self.new_user_button=((By.XPATH, "//a[@title='فرد جدید']"))
+        self.new_user_button=((By.XPATH, "(//a[contains(text(),'جدید')])[1]"))
+
         self.user_firstname_field = ((By.ID,"FirstName"))
         self.user_lastname_field = ((By.ID,"LastName"))
         self.user_meli_field = ((By.ID,"Meli"))
-        self.user_girl_field = ((By.ID,"FirstName"))
+        self.user_sex_field = ((By.ID, 'Girl'))
         self.user_barcode_field = ((By.ID,"stid"))
         self.user_cardnumber_field = ((By.ID,"CardNumber"))
         self.user_expiredate_field = ((By.ID,"ExpireDate"))
         self.user_username_field = ((By.ID,"txtusername1"))
         self.user_password_field = ((By.ID,"txtpassword1"))
         self.user_repassword_field = ((By.ID,"RePassword"))
-        self.user_role_field = ((By.ID,"Roles"))
+        #self.user_role_field = ((By.ID,"Roles"))
+        #self.user_role_field = ((By.CSS_SELECTOR,".ng-isolate-scope ng-not-empty ng-valid .select2-selection--single"))
         self.user_passwordfaramooshi_field = ((By.ID,"PasswordFaramooshi"))
         self.user_personelli_field = ((By.ID,"Personneli"))
         self.active_fromdate_field = ((By.ID,"txtFromDate"))
         self.active_todate_field = ((By.ID,"txtToDate"))
-        
-
-        #select_element = driver.find_element(By.NAME, 'selectomatic')
-        #select = Select(select_element)
-        #select.select_by_visible_text('Four')
 
         self.submit_button = ((By.XPATH,"(//button[contains(text(),'ثبت')])[1]"))
 
 
+        #//////////////////////////////////////////////
+    # Open the dropdown
 
-    def new_group(self):
+        #/////////////////////////////////////////////
+
+
+
+    def new_user(self):
         Side_Menu = MainMenu(self.driver)
         Side_Menu.create_user_menu()
         wait = WebDriverWait(self.driver, 5)
         wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//a[@title='فرد جدید']"))
+        EC.visibility_of_element_located((By.XPATH, "(//a[contains(text(),'جدید')])[1]"))
         )
 
         self.driver.find_element(*self.new_user_button).click()
         wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//input[@id='Groupname']"))
+        EC.visibility_of_element_located((By.ID,"FirstName"))
         )
 
 
-    def enter_group_name(self,group_name):
-        self.driver.find_element(*self.group_name_field).send_keys(group_name)
+    def enter_user_firstname(self,firstname):
+        self.driver.find_element(*self.user_firstname_field).send_keys(firstname)
 
-    def enter_group_MinIncreaseCredit(self,MinIncreaseCredit):
-        self.driver.find_element(*self.MinIncreaseCredit_field).send_keys(MinIncreaseCredit)
+    def enter_user_lastname(self,lastname):
+        self.driver.find_element(*self.user_lastname_field).send_keys(lastname)
 
-    def enter_group_MaxIncreaseCredit(self,MaxIncreaseCredit):
-        self.driver.find_element(*self.MaxIncreaseCredit_field).send_keys(MaxIncreaseCredit)
+    def enter_user_meli(self,meli):
+        self.driver.find_element(*self.user_meli_field).send_keys(meli)
+    
+    def enter_user_sex(self,sex):
+        self.driver.find_element(*self.user_sex_field)
+        select = Select(self.driver.find_element(*self.user_sex_field))
+        select.select_by_visible_text(sex)
+
+    def enter_user_barcode(self,barcode):
+        self.driver.find_element(*self.user_barcode_field).send_keys(barcode)
+    
+    def enter_user_cardnumber(self,cardnumber):
+        self.driver.find_element(*self.user_cardnumber_field).send_keys(cardnumber)
+
+    def enter_user_expiredate(self,expiredate):
+        self.driver.find_element(*self.user_expiredate_field).send_keys(expiredate)
+    
+    def enter_user_username(self,username):
+        self.driver.find_element(*self.user_username_field).send_keys(username)
+    
+    def enter_user_password(self,password):
+        self.driver.find_element(*self.user_password_field).send_keys(password)
+
+    def enter_user_repassword(self,repassword):
+        self.driver.find_element(*self.user_repassword_field).send_keys(repassword)
+
+    def enter_user_role(self,role):
+        #self.driver.find_element(*self.user_role_field).send_keys(role)
+        wait = WebDriverWait(self.driver, 5)
+        dropdown = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR,".ng-isolate-scope ng-not-empty ng-valid .select2-selection--single"))
+                    )
+        dropdown.click()
+
+# Click an option by its visible text
+        option = wait.until(
+        EC.element_to_be_clickable((By.XPATH,f"//li[contains(@class,'select2-results__option') and normalize-space()='{role}']"))
+                            )
+        option.click()
+
+    def enter_user_passwordfaramooshi(self,passwordfaramooshi):
+        self.driver.find_element(*self.user_passwordfaramooshi_field).send_keys(passwordfaramooshi)
+
+    def enter_user_personelli(self,personelli):
+        self.driver.find_element(*self.user_personelli_field).send_keys(personelli)
+
+    def enter_user_active_fromdate(self,active_fromdate):
+        self.driver.find_element(*self.active_fromdate_field).send_keys(active_fromdate)
+
+    def enter_user_active_todate(self,active_todate):
+        self.driver.find_element(*self.active_todate_field).send_keys(active_todate)
+
+    
     
     def click_create(self):
         self.driver.find_element(*self.submit_button).click()
 
 
 
-    def create_new_group(self,group_name,MinIncreaseCredit,MaxIncreaseCredit):
-        self.enter_group_name(group_name)
-        self.enter_group_MinIncreaseCredit(MinIncreaseCredit)
-        self.enter_group_MaxIncreaseCredit(MaxIncreaseCredit)
+    def create_new_user(self,firstname,lastname,meli,sex,barcode,cardnumber,expiredate,username,password,repassword,role,passwordfaramooshi,personelli,active_fromdate,active_todate):
+        self.enter_user_firstname(firstname)
+        self.enter_user_lastname(lastname)
+        self.enter_user_meli(meli)
+        self.enter_user_sex(sex)
+        self.enter_user_barcode(barcode)
+        self.enter_user_cardnumber(cardnumber)
+        self.enter_user_expiredate(expiredate)
+        self.enter_user_username(username)
+        self.enter_user_password(password)
+        self.enter_user_repassword(repassword)
+        self.enter_user_role(role)
+        self.enter_user_passwordfaramooshi(passwordfaramooshi)
+        self.enter_user_personelli(personelli)
+        self.enter_user_active_fromdate(active_fromdate)
+        self.enter_user_active_todate(active_todate)
         self.click_create()
         
--- TestScripts/Test_CreateUser.py
+++ TestScripts/Test_CreateUser.py
@@ -3,28 +3,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PageObjects.LoginPage_POM import LoginPage
 from Components.Main_Menu import MainMenu
-from PageObjects.Create_Group_POM import CreateGroupPage
+from PageObjects.Create_User_POM import CreateUserPage
 
 
-def test_create_new_group(setup):
+def test_create_new_user(setup):
     driver=setup
     Login_page = LoginPage(driver)
     Main_menu = MainMenu(driver)
-    Create_group = CreateGroupPage(driver)
+    Create_user = CreateUserPage(driver)
 
     Login_page.login("supervisor","1")
     wait = WebDriverWait(driver, 5)
     wait.until(
     EC.visibility_of_element_located((By.ID, "main-menu"))
     )
-    Main_menu.create_group_menu()
+    Main_menu.create_user_menu()
     wait.until(
     EC.visibility_of_element_located((By.XPATH, '//a[normalize-space()="جدید"]'))
     )
-    Create_group.new_group()
-    Create_group.create_new_group("sel test","10000","100000")
+    Create_user.new_user()
+    Create_user.create_new_user("عماد","عمادی","1111111141","مرد","32","32","1408/04/01","user32","Aa@12345","Aa@12345","کاربر","1","32","1404/04/01","1408/04/01")
     New_created_group=wait.until(
-    EC.visibility_of_element_located((By.XPATH, "(//td[contains(text(),'sel test')])[1]"))
+    EC.visibility_of_element_located((By.XPATH, "(//td[@class='ng-binding'][normalize-space()='Supervisor'])[1]"))
     )
     
-    assert New_created_group.is_displayed(),"Create group should be successful but it is not"
+    assert New_created_group.is_displayed(),"Create user should be successful but it is not"
