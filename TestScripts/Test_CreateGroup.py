f69fb46eb9ec736b9c5d28b0b23e74f250b276ef
dev-masoud <masoudrezaie1999@gmail.com>
2026-08-02 22:08:11 +0330

    create group assert fixed

--- TestScripts/Test_CreateGroup.py
+++ TestScripts/Test_CreateGroup.py
@@ -6,8 +6,11 @@ from Helpers.data_loader import DataLoader
 def test_create_new_group(driver):
     login_page = LoginPage(driver)
     group_management_page = GroupManagementPage(driver)
-    group_data = DataLoader.load_group("default")
     login_page.login(**DataLoader.load_login("admin_pass"))
-    group_management_page.create_group(**DataLoader.load_group("default"))
 
-    assert group_management_page.is_group_created(group_data["name"]),"Create group should be successful but it is not"
+    success, error = group_management_page.create_group(**DataLoader.load_group("default"))
+    assert success, (
+        f"Create food failed. Server message: {error}"
+        if error
+        else "Create food should be successful but it was not."
+    )
