5ebb5b0d6eabc9a9aa6adba62205dd728fe913a1
support <support@jahan.gostar>
2026-08-11 11:31:49 +0330

    .

--- TestScripts/Test_PrimaryDefinitions.py
+++ TestScripts/Test_PrimaryDefinitions.py
@@ -8,8 +8,6 @@ from PageObjects.Create_Food_POM import FoodManagementPage
 from PageObjects.Create_FoodPrice_POM import FoodPriceManagementPage
 from Helpers.data_loader import DataLoader
 
-import time
-
 
 def test_primary_definitions(driver):
     login_page = LoginPage(driver)
@@ -22,6 +20,7 @@ def test_primary_definitions(driver):
     food_price_management_page = FoodPriceManagementPage(driver)
 
     login_page.login(**DataLoader.load_login("admin_pass"))
+    
     assert group_management_page.create_group(**DataLoader.load_group("default"))
     
     assert user_management_page.create_user(**DataLoader.load_user("default"))
@@ -34,4 +33,5 @@ def test_primary_definitions(driver):
     
     assert food_management_page.create_food(**DataLoader.load_food("default"))
     driver.refresh()
+    
     assert food_price_management_page.create_new_foodprice(**DataLoader.load_foodprice("default"))
