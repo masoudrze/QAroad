3628869141836ba3a9b003aeba1dd90f0c3fb8b4
support <support@jahan.gostar>
2026-08-18 14:43:08 +0330

    add allure epic,feature,story,title to logintest

--- TestScripts/Test_LoginClass.py
+++ TestScripts/Test_LoginClass.py
@@ -1,11 +1,14 @@
-import pytest
+import allure
 
 from Helpers.data_loader import DataLoader
 from PageObjects.LoginPage_POM import LoginPage
 from PageObjects.UserIndex_Nav_POM import UserIndexNav
 
-
-@pytest.mark.negative
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("Admin User Entered Wrong Credential")
+@allure.title("Invalid admin login")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_invalid_admin_login(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("admin_fail"))
@@ -13,7 +16,11 @@ def test_invalid_admin_login(driver):
     assert expected_text in login_page.login_failed_message()
 
 
-@pytest.mark.negative
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("simple User Entered Wrong Credential")
+@allure.title("Invalid user login")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_invalid_user_login(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("user_fail"))
@@ -21,7 +28,11 @@ def test_invalid_user_login(driver):
     assert expected_text in login_page.login_failed_message()
 
 
-@pytest.mark.smoke
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("Admin User Correct login")
+@allure.title("valid admin login")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_valid_admin_login(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("admin_pass"))
@@ -30,7 +41,11 @@ def test_valid_admin_login(driver):
     )
 
 
-@pytest.mark.smoke
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("Simple User Correct login")
+@allure.title("valid user login")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_valid_user_login(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("user_pass"))
@@ -39,7 +54,11 @@ def test_valid_user_login(driver):
     )
 
 
-@pytest.mark.smoke
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("Admin User Logged in and Logged out")
+@allure.title("valid admin login logout")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_valid_admin_login_logout(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("admin_pass"))
@@ -48,7 +67,11 @@ def test_valid_admin_login_logout(driver):
     assert logout.is_logged_out(), "logout should be successful but it is not"
 
 
-@pytest.mark.smoke
+@allure.epic("Login Management")
+@allure.feature("User Login")
+@allure.story("simple User Logged in and Logged out")
+@allure.title("valid simple login logout")
+@allure.severity(allure.severity_level.BLOCKER)
 def test_valid_user_login_logout(driver):
     login_page = LoginPage(driver)
     login_page.login(**DataLoader.load_login("user_pass"))
